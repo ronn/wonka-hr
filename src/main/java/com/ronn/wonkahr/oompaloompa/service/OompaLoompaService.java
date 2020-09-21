7e1663cf4ae152f2150fdab378c0ce3d95da0fb4
ronn <ronnie@nextdigital.es>
2020-09-21 22:29:16 +0200

    Define all domain service functions

--- src/main/java/com/ronn/wonkahr/oompaloompa/service/OompaLoompaService.java
+++ src/main/java/com/ronn/wonkahr/oompaloompa/service/OompaLoompaService.java
@@ -2,18 +2,36 @@ package com.ronn.wonkahr.oompaloompa.service;
 
 import com.ronn.wonkahr.oompaloompa.model.OompaLoompa;
 import com.ronn.wonkahr.oompaloompa.repository.OompaLoompaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OompaLoompaService {
 
-  @Autowired
-  OompaLoompaRepository repo;
+  private final OompaLoompaRepository repo;
+
+  public OompaLoompaService(OompaLoompaRepository repo) {
+    this.repo = repo;
+  }
 
   public List<OompaLoompa> getAll(){
     return repo.findAll();
+  }
+
+  public Optional<Object> save(OompaLoompa oompaLoompa){
+    return getBy(oompaLoompa.getName())
+        .map(ol -> Optional.empty())
+        .orElse(Optional.of(repo.save(oompaLoompa)));
+  }
+
+  public Optional<OompaLoompa> getBy(String name){
+    return repo.findOompaLoompaByName(name);
+  }
+
+  public Optional<Object> update(OompaLoompa oompaLoompa){
+    return getBy(oompaLoompa.getName())
+        .map(x -> repo.save(oompaLoompa));
   }
 }
