b3b664ba27c2603cc064f5b2dd2ede88fea6e2ec
ronn <ronnie@nextdigital.es>
2020-09-21 22:30:43 +0200

    Create model to expose list of Ols

---
+++ src/main/java/com/ronn/wonkahr/web/controller/model/OompaLoompaDto.java
@@ -0,0 +1,32 @@
+package com.ronn.wonkahr.web.controller.model;
+
+import com.ronn.wonkahr.oompaloompa.model.OompaLoompa;
+
+public class OompaLoompaDto {
+
+  private final String name;
+  private final Integer age;
+  private final String job;
+
+  public static OompaLoompaDto fromEntity(OompaLoompa oompaLoompa) {
+    return new OompaLoompaDto(oompaLoompa.getName(), oompaLoompa.getAge(), oompaLoompa.getJob());
+  }
+
+  private OompaLoompaDto(String name, Integer age, String job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public String getJob() {
+    return job;
+  }
+}
