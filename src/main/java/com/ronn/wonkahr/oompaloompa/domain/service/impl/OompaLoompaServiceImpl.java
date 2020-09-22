2bd6bffbaba0f9bf3460d069fa88a06a0cbf4073
ronn <ronnie@nextdigital.es>
2020-09-22 19:27:41 +0200

    Create CRUD operations in serice

--- src/main/java/com/ronn/wonkahr/oompaloompa/domain/service/impl/OompaLoompaServiceImpl.java
+++ src/main/java/com/ronn/wonkahr/oompaloompa/domain/service/impl/OompaLoompaServiceImpl.java
@@ -1,14 +1,15 @@
-package com.ronn.wonkahr.oompaloompa.service;
+package com.ronn.wonkahr.oompaloompa.domain.service.impl;
 
-import com.ronn.wonkahr.oompaloompa.model.OompaLoompa;
+import com.ronn.wonkahr.oompaloompa.domain.model.OompaLoompa;
 import com.ronn.wonkahr.oompaloompa.repository.OompaLoompaRepository;
+import com.ronn.wonkahr.oompaloompa.domain.service.OompaLoompaService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class OompaLoompaServiceImpl {
+public class OompaLoompaServiceImpl implements OompaLoompaService {
 
   private final OompaLoompaRepository repo;
 
@@ -16,21 +17,21 @@ public class OompaLoompaServiceImpl {
     this.repo = repo;
   }
 
-  public List<OompaLoompa> getAll(){
+  public List<OompaLoompa> getAll() {
     return repo.findAll();
   }
 
-  public Optional<Object> save(OompaLoompa oompaLoompa){
+  public Optional<Object> save(OompaLoompa oompaLoompa) {
     return getBy(oompaLoompa.getName())
         .map(ol -> Optional.empty())
         .orElse(Optional.of(repo.save(oompaLoompa)));
   }
 
-  public Optional<OompaLoompa> getBy(String name){
+  public Optional<OompaLoompa> getBy(String name) {
     return repo.findOompaLoompaByName(name);
   }
 
-  public Optional<Object> update(OompaLoompa oompaLoompa){
+  public Optional<Object> update(OompaLoompa oompaLoompa) {
     return getBy(oompaLoompa.getName())
         .map(x -> repo.save(oompaLoompa));
   }
