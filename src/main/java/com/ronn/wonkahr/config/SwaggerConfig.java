6c058b0047bf8901160fa1ed4677a914a2178870
ronn <ronnie@nextdigital.es>
2020-09-24 09:02:19 +0200

    Configure Swagger

--- src/main/java/com/ronn/wonkahr/config/SwaggerConfig.java
+++ src/main/java/com/ronn/wonkahr/config/SwaggerConfig.java
@@ -1,2 +1,38 @@
-package com.ronn.wonkahr.config;public class SwaggerConfig {
+package com.ronn.wonkahr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(regex("/oompa-loompa.*"))
+        .build()
+        .apiInfo(metaInfo());
+  }
+
+  private ApiInfo metaInfo() {
+    return new ApiInfo(
+        "Willy Wonka's Employee Management",
+        "An app to manage Factory's Ommpa-Loompas",
+        "1.0",
+        "Terms of Service",
+        "ronnie-filth@hotmail.com",
+        "Apache License Version 2.0",
+        "https://www.apache.org/licesen.html"
+    );
+  }
 }
